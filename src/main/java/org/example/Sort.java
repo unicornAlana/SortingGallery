e8480a4ad3822a15766253a1c7f25d8f607ded11
bass.alana <bass.alana@vashonsd.org>
2022-03-23 11:06:44 -0700

    Finished Merge

--- src/main/java/org/example/Sort.java
+++ src/main/java/org/example/Sort.java
@@ -4,10 +4,22 @@ public class Sort {
 
     /**
      * Should take in an array of two ints and changes it
-     * to arrray with the numbers from smallest to largest.
+     * to array with the numbers from smallest to largest.
      * @param nums
      */
-    public static void sortTwo(int[] nums) {
+    public static void sortTwo(int[] nums)
+    {
+        if (nums[0]>nums[1])
+        {
+            int temp = nums[0];
+            nums[0] = nums[1];
+            nums[1] = temp;
+        }
+        else
+        {
+            nums[0] = nums[0];
+            nums[1] = nums[1];
+        }
 
     }
 
@@ -18,7 +30,39 @@ public class Sort {
      * @param array1 - A sorted list of numbers that is the size of half the length of the inputArray
      * @param array2 - A sorted list of numbers that is the size of half the length of the inputArray
      */
-    public static void merge(int[] inputArray, int[] array1, int [] array2) {
+    public static void merge(int[] inputArray, int[] array1, int [] array2)
+    {
+        int i = 0;
+        int a = 0;
+        int b = 0;
+
+        while (a < array1.length && b < array2.length)
+        {
+            if (array1[a] < array2[b])
+            {
+                inputArray[i] = array1[a];
+                a++;
+            }
+            else
+            {
+                inputArray[i] = array2[b];
+                b++;
+            }
+            i++;
+        }
+        while(a < array1.length) {
+            //add them all in in order.
+            inputArray[i] = array1[a];
+            a++;
+            i++;
+        }
+
+        while(b < array2.length) {
+            //add them all in in order.
+            inputArray[i] = array2[b];
+            b++;
+            i++;
+        }
 
     }
 }
